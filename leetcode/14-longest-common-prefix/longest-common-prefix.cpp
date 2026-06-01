1b11e2b414658a872efdab102081f018704d0bf2
Akshaj Mishra <141712076+Akshaj-mishra@users.noreply.github.com>
2026-06-01 14:54:54 +0530

    added imports

--- leetcode/14-longest-common-prefix/longest-common-prefix.cpp
+++ leetcode/14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,3 +1,8 @@
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
--- leetcode/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ leetcode/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <climits>
+using namespace std;
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
