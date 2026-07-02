320ba0615b52bbc3ed3ab8d241e72a5272e2db20
Akshaj Mishra <141712076+Akshaj-mishra@users.noreply.github.com>
2026-07-03 00:32:22 +0530

    Time: 3 ms (37.80%) | Memory: 42.3 MB (17.09%) - LeetSync

--- leetcode/238-product-of-array-except-self/product-of-array-except-self.cpp
+++ leetcode/238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
+        vector<int> presun(n,1);
+        vector<int> postsun(n,1);
+        vector<int> ans(n,1);
 
-        vector<int> presum(n, 1);
-        vector<int> postsum(n, 1);
-        vector<int> ans(n);
 
-        for (int i = 1; i < n; i++) {
-            presum[i] = presum[i - 1] * nums[i - 1];
+        for (int i = 1 ; i<n; i++){
+            presun[i] = presun[i-1] * nums[i-1];
         }
 
-        for (int j = n - 2; j >= 0; j--) {
-            postsum[j] = postsum[j + 1] * nums[j + 1];
+        for (int j = n-2 ; j>=0; j--){
+            postsun[j] = postsun[j+1] * nums[j+1];
         }
 
-        for (int i = 0; i < n; i++) {
-            ans[i] = presum[i] * postsum[i];
+        for (int k = 0 ; k<n; k++){
+            ans[k] = presun[k] * postsun[k];
         }
+
 
         return ans;
     }
