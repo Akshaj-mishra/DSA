f5f4290f6e7aadd2fe6332111c5cf7ea94439c34
Akshaj Mishra <141712076+Akshaj-mishra@users.noreply.github.com>
2026-07-04 16:43:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (59.62%) - LeetSync

--- leetcode/152-maximum-product-subarray/maximum-product-subarray.cpp
+++ leetcode/152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -5,17 +5,19 @@ public:
         if (n == 0) return 0;
 
         int ans = nums[0];
-        int maxProd = nums[0];
-        int minProd = nums[0];
+        int maximum = nums[0];
+        int minimum = nums[0];
 
         for (int i = 1; i < n; ++i) {
             if (nums[i] < 0) {
-                swap(maxProd, minProd);
+                swap(maximum, minimum);
             }
-            maxProd = max(nums[i], maxProd * nums[i]);
-            minProd = min(nums[i], minProd * nums[i]);
+
+
+            maximum = max(nums[i], maximum * nums[i]);
+            minimum = min(nums[i], minimum * nums[i]);
             
-            ans = max(ans, maxProd);
+            ans = max(ans, maximum);
         }
 
         return ans;
