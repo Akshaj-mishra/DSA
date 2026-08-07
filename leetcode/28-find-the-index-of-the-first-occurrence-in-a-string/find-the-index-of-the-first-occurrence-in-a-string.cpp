667c8ac906050680b151a2f6f052122d46218dbd
Akshaj Mishra <141712076+Akshaj-mishra@users.noreply.github.com>
2026-08-07 15:35:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (16.18%) - LeetSync

--- leetcode/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ leetcode/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,41 +1,11 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-    //     if (haystack == needle){
-    //         return 0;
-    //     }
-    //     int l = 0;
-    //     int r = needle.size()-1;
-    //     int n = haystack.size()-1;
-    //     bool val = false;
-    //     while (r < n){
-
-    //         if (haystack[l] == needle[0]){
-    //             string str = "";
-    //             for (int i = l; i <=r; i++){
-    //                 str += haystack[i];
-    //             }
-    //             if (str == needle){
-    //                 val = true;
-    //                 break;
-    //             }
-    //         }
-            
-    //         l++;
-    //         r++;
-            
-            
-    //     }
-    //     if (val){
-    //         return l;
-    //     }
-    //    return -1;
-
-
-    int i = haystack.find(needle);
-    if (i != haystack.npos){
-        return i;
-    }
-    return -1;
+        for (int i = 0; i + needle.size() <= haystack.size(); i++) {
+            if (haystack.substr(i, needle.size()) == needle){
+                return i;
+            }
+        }
+        return -1;
     }
 };
